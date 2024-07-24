5aa94cc2c6e9a05d8a32aaf83ac9830b6b634a39
Franklin <franloclounch2@gmail.com>
2024-07-23 21:18:25 -0500

    iniciar Sesión completo.

--- app/src/main/java/com/example/borrarahoratambien/Login.java
+++ app/src/main/java/com/example/borrarahoratambien/Login.java
@@ -1,24 +1,137 @@
 package com.example.borrarahoratambien;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Login extends AppCompatActivity {
+    EditText CorreoLogin,PassLogin;
+    Button Btn_loging;
+    TextView UsuarioNuevoTXT;
+
+    ProgressDialog progressDialog;
+    FirebaseAuth firebaseAuth;
+    //validar los datos
+    String correo ="",password="";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("Login");
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
+        CorreoLogin = findViewById(R.id.CorreoLogin);
+        PassLogin = findViewById(R.id.PassLogin);
+        Btn_loging = findViewById(R.id.Btn_loging);
+        UsuarioNuevoTXT= findViewById(R.id.UsuarioNuevoTXT);
+
+        firebaseAuth= FirebaseAuth.getInstance();
+
+        progressDialog = new ProgressDialog(Login.this);
+        progressDialog.setTitle("Espere por favor");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        Btn_loging.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validarDatos();
+            }
+        });
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        UsuarioNuevoTXT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, Registro.class));
+            }
+        });
+    }
+
+    private void validarDatos() {
+        correo= CorreoLogin.getText().toString();
+        password = PassLogin.getText().toString();
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(correo).matches()){
+            Toast.makeText(this, "Correo inválido", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Ingrese contraseña", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            LoginDeUsuario();
+        }
+    }
+
+    private void LoginDeUsuario() {
+        progressDialog.setMessage("Iniciamos sesión ...");
+        progressDialog.show();
+        firebaseAuth.signInWithEmailAndPassword(correo,password)
+                .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        progressDialog.dismiss();
+                        FirebaseUser user = firebaseAuth.getCurrentUser();
+                        startActivity(new Intent(Login.this, MenuPrincipal.class));
+                        Toast.makeText(Login.this, "Bienvenido(a): " +user.getEmail(), Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                    else {
+                        progressDialog.dismiss();
+                        Toast.makeText(Login.this, "Verifique si el correo y la contraseña es correcto", Toast.LENGTH_SHORT).show();
+                    }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Login.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
