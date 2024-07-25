03522e2a629cf04da073c09c6fe638d552544bde
Franklin <franloclounch2@gmail.com>
2024-07-24 19:54:26 -0500

    hasta 15 video.

--- app/src/main/java/com/example/borrarahoratambien/MenuPrincipal.java
+++ app/src/main/java/com/example/borrarahoratambien/MenuPrincipal.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -14,36 +17,44 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MenuPrincipal extends AppCompatActivity {
 
-    Button CerrarSesion;
-    FirebaseAuth firebaseAuth;
-    FirebaseUser user;
+    private Button CerrarSesion;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser user;
 
+    private TextView NombresPrincipal, CorreoPrincipal;
+    private ProgressBar progressBarDatos;
+
+    private DatabaseReference usuarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu_principal);
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setTitle("H&Q Tech");
+        }
 
-        CerrarSesion = findViewById(R.id.CerrarSesión);
+        NombresPrincipal = findViewById(R.id.NombresPrincipal);
+        CorreoPrincipal = findViewById(R.id.CorreoPrincipal);
+        progressBarDatos = findViewById(R.id.progressBarDatos);
+
+        usuarios = FirebaseDatabase.getInstance().getReference("usuarios");
+
+        CerrarSesion = findViewById(R.id.CerrarSesión); // Asegúrate de que este ID coincida con el definido en tu archivo XML.
         firebaseAuth = FirebaseAuth.getInstance();
         user = firebaseAuth.getCurrentUser();
 
-
-        CerrarSesion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SalirAplicacion();
-            }
-        });
-
-
-
-
+        CerrarSesion.setOnClickListener(view -> SalirAplicacion());
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -52,9 +63,56 @@ public class MenuPrincipal extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ComprobarInicioSesion();
+    }
+
+    private void ComprobarInicioSesion() {
+        if (user != null) {
+            // El usuario ha iniciado sesión
+            CargaDeDatos();
+        } else {
+            // Lo dirigirá al MainActivity
+            startActivity(new Intent(MenuPrincipal.this, MainActivity.class));
+            finish();
+        }
+    }
+
+    private void CargaDeDatos() {
+        usuarios.child(user.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // Si el usuario existe
+                if (snapshot.exists()) {
+                    // El progressBar se oculta
+                    progressBarDatos.setVisibility(View.GONE);
+                    // Los textView se muestran
+                    NombresPrincipal.setVisibility(View.VISIBLE);
+                    CorreoPrincipal.setVisibility(View.VISIBLE);
+
+                    // Obtener los datos
+                    String nombres = "" + snapshot.child("nombres").getValue();
+                    String correo = "" + snapshot.child("correo").getValue();
+
+                    // Setear los datos en los respectivos TextView
+                    NombresPrincipal.setText(nombres);
+                    CorreoPrincipal.setText(correo);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Manejar el error aquí si es necesario
+            }
+        });
+    }
+
     private void SalirAplicacion() {
         firebaseAuth.signOut();
         startActivity(new Intent(MenuPrincipal.this, MainActivity.class));
-        Toast.makeText(this, "Cerraste sesión exitoamente", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Cerraste sesión exitosamente", Toast.LENGTH_SHORT).show();
+        finish();
     }
 }
